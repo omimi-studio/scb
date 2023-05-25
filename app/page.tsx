6bc8aef5979f1ce40b64ca77c643743aea03e70d
Baranabas Oretan <barnabas.oretan@gmail.com>
2023-05-25 07:52:56 +0100

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,280 @@
-import Image from 'next/image'
+import Image from "next/image";
+import bnb from "../images/icon-3.svg";
+import logo from "../images/logo.png";
+import swap1 from "../images/swap-1.svg";
+import swap2 from "../images/swap-2.svg";
+import swap6 from "../images/swap-6.svg";
+import icon11 from "../images/icon-11.png";
+import icon15 from "../images/icon-15.png";
+import icon16 from "../images/icon-16.png";
+import icon17 from "../images/icon-17.png";
+import icon18 from "../images/icon-18.png";
+import icon19 from "../images/icon-19.png";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="">
+      <div className="bg-image dashboard">
+        <div className="main">
+          <header>
+            <div className="container">
+              <div className="row">
+                <nav className="navbar navbar-expand-md w-100">
+                  {/* <!-- Brand --> */}
+                  <a className="navbar-brand" href="#">
+                    <Image
+                      width={80}
+                      height={50}
+                      src={logo}
+                      alt=""
+                      className="img-fluid"
+                    />
+                  </a>
+
+                  {/* <!-- Toggler/collapsibe Button --> */}
+                  <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#collapsibleNavbar"
+                  >
+                    <span className="navbar-toggler-icon"></span>
+                  </button>
+
+                  {/* <!-- Navbar links --> */}
+                  <div
+                    className="navbar-collapse"
+                    id="collapsibleNavbar"
+                  >
+                    <ul className="navbar-nav">
+                      <li className="nav-item">
+                        <a className="nav-link active" href="#">
+                          My Account
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a className="nav-link" href="#">
+                          Setting
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a className="nav-link" href="#">
+                          Logout
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </nav>
+              </div>
+            </div>
+          </header>
+          <div className="container">
+            <div className="swap-block">
+              <div className="top-bg">
+                <div className="text-a">Deposit</div>
+                <div className="text-b">Deposit BUSD to your SCB Wallet</div>
+                <div className="text-c">
+                  <a href="#" className="icon-link">
+                    <Image width={20} height={20} src={swap1} alt="" />
+                  </a>
+                  <a href="#" className="icon-link">
+                    <Image width={20} height={20} src={swap2} alt="" />
+                  </a>
+                </div>
+              </div>
+              <div className="area">
+                <div className="block-a">
+                  <div className="row align-items-center">
+                    <div className=" col-auto">
+                      <span className="icon-a p-2">
+                        <Image width={40} height={40} src={bnb} alt="" />
+                      </span>
+                      <div
+                        className="select"
+                        data-toggle="modal"
+                        data-target="#selectToken"
+                      >
+                        BNB
+                      </div>
+                    </div>
+                    <div className="col">
+                      <div className="bal-text">Balance: 0.1478542</div>
+                    </div>
+                  </div>
+                  <div className="text-msg">
+                    <textarea>0.0</textarea>
+                    <div className="av-row"></div>
+                  </div>
+                </div>
+                {/* <div className="down-arrow">
+                            <span className="icon">
+                                <Image width={40} height={40} src="../images/swap-5.svg" alt="" />
+                            </span>
+                        </div>
+                        <div className="block-b">
+                            <div className="row align-items-center">
+                                <div className="col-auto">
+                                    <span className="icon-a"><Image width={40} height={40} src="../images/icon-3.svg" alt="" /></span>
+                                    <select className="select">
+                                        <option>GEMS</option>
+                                        <option>GEMS</option>
+                                        <option>GEMS</option>
+                                    </select>
+                                    <span className="icon-b"><Image width={40} height={40} src="../images/swap-3.svg" alt="" /></span>
+                                    <span className="icon-b"><Image width={40} height={40} src="../images/swap-4.svg" alt="" /></span>
+                                </div>
+                                <div className="col">
+                                    <div className="bal-text">Balance: 0</div>
+                                </div>
+                            </div>
+                            <div className="text-msg">
+                                <textarea>0.0</textarea>
+                            </div>
+                        </div>*/}
+                <div className="block-c">
+                  Minimum Received: <strong> 20.9147 BUSD</strong>
+                  <br />
+                  <strong className="bb">WBNB &gt; SCB &gt; BUSD</strong>
+                </div>
+                <button className="btn btn-primary btn-md">Deposit</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="modal select-token" id="selectToken">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="top-bg">Select A Token</div>
+            <span className="close" data-dismiss="modal">
+              <Image width={20} height={20} src={swap6} alt="" />
+            </span>
+            <div className="modal-body">
+              <div className="search">
+                <input
+                  type="text"
+                  placeholder="Search name or paste address"
+                  name=""
+                />
+              </div>
+
+              <ul className="item-list">
+                <li className="select">
+                  <div className="row no-gutters align-items-center">
+                    <div className="col-auto">
+                      <Image width={40} height={40} src={icon11} alt="" />
+                    </div>
+                    <div className="col">
+                      <strong>BNB</strong>
+                      <br />
+                      Binance Chain Native Token
+                    </div>
+                    <div className="col-auto">
+                      <strong>0.1475</strong>
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <div className="row no-gutters align-items-center">
+                    <div className="col-auto">
+                      <Image width={40} height={40} src={icon15} alt="" />
+                    </div>
+                    <div className="col">
+                      <strong>SCB</strong>
+                      <br />
+                      Added by user + Seismic CB
+                    </div>
+                    <div className="col-auto">
+                      <strong>0.1475</strong>
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <div className="row no-gutters align-items-center">
+                    <div className="col-auto">
+                      <Image width={40} height={40} src={icon15} alt="" />
+                    </div>
+                    <div className="col">
+                      <strong>BPAY</strong>
+                      <br />
+                      BPAY Network
+                    </div>
+                    <div className="col-auto">
+                      <strong>0.1475</strong>
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <div className="row no-gutters align-items-center">
+                    <div className="col-auto">
+                      <Image width={40} height={40} src={icon16} alt="" />
+                    </div>
+                    <div className="col">
+                      <strong>ACH</strong>
+                      <br />
+                      Alchemy Token
+                    </div>
+                    <div className="col-auto">
+                      <strong>0.1475</strong>
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <div className="row no-gutters align-items-center">
+                    <div className="col-auto">
+                      <Image width={40} height={40} src={icon17} alt="" />
+                    </div>
+                    <div className="col">
+                      <strong>ADA</strong>
+                      <br />
+                      Cardano Token
+                    </div>
+                    <div className="col-auto">
+                      <strong>0.1475</strong>
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <div className="row no-gutters align-items-center">
+                    <div className="col-auto">
+                      <Image width={40} height={40} src={icon18} alt="" />
+                    </div>
+                    <div className="col">
+                      <strong>ADX</strong>
+                      <br />
+                      AdEX Network
+                    </div>
+                    <div className="col-auto">
+                      <strong>0.1475</strong>
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <div className="row no-gutters align-items-center">
+                    <div className="col-auto">
+                      <Image width={40} height={40} src={icon19} alt="" />
+                    </div>
+                    <div className="col">
+                      <strong>EUR</strong>
+                      <br />
+                      agEUR
+                    </div>
+                    <div className="col-auto">
+                      <strong>0.1475</strong>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <div className="bottom-bg">Manage Tokens</div>
+          </div>
+        </div>
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     </main>
-  )
+  );
 }
